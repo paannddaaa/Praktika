467cda9efceae9900e2601415fd457206910758f
paannddaaa <168432993+paannddaaa@users.noreply.github.com>
2024-10-30 01:10:35 +0300

    Add files via upload

--- NN2.py
+++ NN2.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_chanels, kernel_size, stride, padding):
+    def __init__(self, in_channels, out_chanels, stride, kernel_size, padding):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_chanels, kernel_size, stride, padding)
         self.bn = nn.BatchNorm2d(out_chanels)
@@ -23,75 +22,43 @@ class InceptionBlock(nn.Module):
             red_5x5,
             out_5x5,
             out_pool,
+            stride,
     ):
         super(InceptionBlock, self).__init__()
-        self.branch1 = ConvBlock(in_channels, out_1x1, kernel_size=1, stride=1, padding=0)
-        self.branch2 = nn.Sequential(
-            ConvBlock(in_channels, red_3x3, kernel_size=1, stride=1, padding=0),
-            ConvBlock(red_3x3, out_3x3, kernel_size=3, stride=1, padding=1)
-        )
-        self.branch3 = nn.Sequential(
-            ConvBlock(in_channels, red_5x5, kernel_size=1, stride=1, padding=0),
-            ConvBlock(red_5x5, out_5x5, kernel_size=5, stride=1, padding=2)
-        )
-        self.branch4 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            ConvBlock(in_channels, out_pool, stride=1, kernel_size=1, padding=0)
-        )
-        #if out_1x1 == 0:
-        # #self.branch1 != ConvBlock(in_channels, out_1x1, kernel_size=1, stride=1, padding=0)
+        self.branches = []
+        if out_1x1:
+            self.branches.append(ConvBlock(in_channels, out_1x1, stride=1, kernel_size=1,  padding=0 ))
+        self.branches.append(nn.Sequential(
+            ConvBlock(in_channels, red_3x3, stride=1, kernel_size=1,  padding=0),
+            ConvBlock(red_3x3, out_3x3, stride, kernel_size=3,  padding=1)))
+        if (red_5x5 and out_5x5):
+            self.branches.append(nn.Sequential(
+                ConvBlock(in_channels, red_5x5, stride=1, kernel_size=1, padding=0),
+                ConvBlock(red_5x5, out_5x5, stride, kernel_size=5,  padding=2)))
+        if out_pool:
+            self.branches.append(nn.Sequential(
+                nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+                ConvBlock(in_channels, out_pool, stride=1, kernel_size=1, padding=0)))
 
     def forward(self, x):
-        branches = (self.branch1, self.branch2, self.branch3, self.branch4)
-        #if self.branch1 == 0:
-        #    branches = (self.branch2, self.branch3, self.branch4)
-        #else:                  это попытки сделать через один InceptionBlock при помощи условий, а не вводить второй класс
-        #    branches = (self.branch1, self.branch2, self.branch3, self.branch4)
-        return torch.cat([branch(x) for branch in branches], 1)
+        return torch.cat([branch(x) for branch in self.branches], 1)
 
-class InceptionBlock2(nn.Module):
-    def __init__(
-            self,
-            in_channels,
-            red_3x3,
-            out_3x3,
-            red_5x5,
-            out_5x5,
-            out_pool,
-    ):
-        super(InceptionBlock2, self).__init__()
-        self.branch2 = nn.Sequential(
-            ConvBlock(in_channels, red_3x3, kernel_size=1, stride=1, padding=0),
-            ConvBlock(red_3x3, out_3x3, kernel_size=3, stride=2, padding=1)
-        )
-        self.branch3 = nn.Sequential(
-            ConvBlock(in_channels, red_5x5, kernel_size=1, stride=1, padding=0),
-            ConvBlock(red_5x5, out_5x5, kernel_size=5, stride=2, padding=2)
-        )
-        self.branch4 = nn.Sequential(
-            torch.linalg.norm(out_5x5, ord=2), ##Как правильно тут чё указать в скобках и главная проблема ещё в дробных числах, укажу где ниже
-            ConvBlock(in_channels, out_pool, stride=2, kernel_size=1, padding=0)
-        )
-
-    def forward(self, x):
-        branches = (self.branch2, self.branch3, self.branch4)
-        return torch.cat([branch(x) for branch in branches], 1)
 
 class NN2(nn.Module):
     def __init__(self):
         super(NN2, self).__init__()
         self.conv1 = ConvBlock(
-            in_channels=3, out_chanels=64, kernel_size=7, stride=2, padding=5,
+            in_channels=3, out_chanels=64, stride=2, kernel_size=7,  padding=5,
         )
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.inception2 = nn.Sequential(
-            ConvBlock(64, 64, kernel_size=1, stride=1, padding=0),
-            ConvBlock(64, 192, kernel_size=3, stride=1, padding=1),
+            ConvBlock(64, 64, stride=1, kernel_size=1,  padding=0),
+            ConvBlock(64, 192, stride=1, kernel_size=3,  padding=1),
         )
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.inseption3a = InceptionBlock(192, 64, 96, 128, 16, 32, 32)
-        self.inseption3b = InceptionBlock(256, 64, 96, 128, 32, 64, 64)
-        self.inseption3c = InceptionBlock2(320, 128, 256.2, 32, 64.2,  64) #тут с дробными числами трабл
+        self.inseption3a = InceptionBlock(192, 64, 96, 128, 16, 32, 32,  1)
+        self.inseption3b = InceptionBlock(256, 64, 96, 128, 32, 64, 64,  1)
+        self.inseption3c = InceptionBlock(320, 0, 128, 256, 32, 64, 0,  2)
 
 
     def forward(self, x):
