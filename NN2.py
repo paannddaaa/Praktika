9259db304d05c99af99eb49d8227ca3c68b0fa2f
paannddaaa <168432993+paannddaaa@users.noreply.github.com>
2024-10-30 13:37:42 +0300

    Add files via upload

--- NN2.py
+++ NN2.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torchsummary import summary
+
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_chanels, stride, kernel_size, padding):
         super(ConvBlock, self).__init__()
@@ -39,6 +41,9 @@ class InceptionBlock(nn.Module):
             self.branches.append(nn.Sequential(
                 nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
                 ConvBlock(in_channels, out_pool, stride=1, kernel_size=1, padding=0)))
+        else:
+            self.branches.append(nn.Sequential(
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1)))
 
     def forward(self, x):
         return torch.cat([branch(x) for branch in self.branches], 1)
@@ -59,7 +64,16 @@ class NN2(nn.Module):
         self.inseption3a = InceptionBlock(192, 64, 96, 128, 16, 32, 32,  1)
         self.inseption3b = InceptionBlock(256, 64, 96, 128, 32, 64, 64,  1)
         self.inseption3c = InceptionBlock(320, 0, 128, 256, 32, 64, 0,  2)
-
+        self.inseption4a = InceptionBlock(640, 256, 96, 192, 32, 64, 128, 1)
+        self.inseption4b = InceptionBlock(640, 224, 112, 224, 32, 64, 128, 1)
+        self.inseption4c = InceptionBlock(640, 192, 128, 256, 32, 64, 128, 1)
+        self.inseption4d = InceptionBlock(640, 160, 144, 288, 32, 64, 128, 1)
+        self.inseption4e = InceptionBlock(640, 0, 160, 256, 64, 128, 0, 2)
+        self.inseption5a = InceptionBlock(1024, 384,192, 384, 48, 128, 128, 1)
+        self.inseption5b = InceptionBlock(1024, 384, 192, 384, 48, 128, 128,1)
+        self.avgpool1 = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc = nn.Linear(in_features=1024, out_features=128)
+        #self.pool3 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -69,6 +83,16 @@ class NN2(nn.Module):
         x = self.inseption3a(x)
         x = self.inseption3b(x)
         x = self.inseption3c(x)
+        x = self.inseption4a(x)
+        x = self.inseption4b(x)
+        x = self.inseption4c(x)
+        x = self.inseption4d(x)
+        x = self.inseption4e(x)
+        x = self.inseption5a(x)
+        x = self.inseption5b(x)
+        x = F.relu(self.avgpool1(x))
+        x = self.fc(x)
+        #x = F.relu(self.pool3(x))
         x = F.tanh(x)
         print(x.shape)
         return F.tanh(x)
@@ -78,3 +102,4 @@ Nelenet2 = NN2()
 input_vector = torch.rand((1, 3, 220, 220))
 Nelenet2(input_vector).shape
 #Nelenet2.lin2.weight.shape
+#summary(Nelenet2, input_size=(3, 220, 220), device="cpu")
